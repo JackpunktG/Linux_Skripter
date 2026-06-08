87d55dac2395a74fdba98a07e3cb131909a92467
jack <jack.goldsbrough@outlook.com>
2026-06-08 08:58:41 +0200

    adding lib's

--- src/main.c
+++ src/main.c
@@ -1,2 +1,19 @@
 #include "stdio.h"
+#include "../lib/easy_args.h"
+#include "../lib/var_types.inc"
 
+
+
+i32 main(i32 argc, char* argv[])
+{
+    Program_Flags flags;
+    if (!set_flags(& flags, argc, argv, ARG_TRUE))
+        return 1;
+
+    print_program_flags(&flags);
+
+    print_args_table();
+
+    if (is_flag_set(&flags, FLAG_TEST))
+    {}
+}
